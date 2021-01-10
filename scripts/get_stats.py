615418edf7bd42734fa0c8d7465edd058e88974b
lucas rg <lucasrg@kth.se>
2021-01-11 00:32:51 +0100

    stats script

---
+++ scripts/get_stats.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+import pandas as pd
+from datetime import datetime
+
+
+PROCESSED_DIR = "data/processed/"  # input
+STATS_DATE_FILE = "results/stats_dates.csv"  # output
+
+
+def name2date(name):
+    return datetime.strptime("".join(name.split('_')[1:3]), "%Y%m%d%H%M%S").date()
+
+
+def main():
+    # Load images
+    files = [file for file in os.listdir(path=PROCESSED_DIR) if file.endswith(".jpg")]
+    images = [Image.open(os.path.join(PROCESSED_DIR, file)) for file in files]
+    # Build table with date occurrences
+    dates = [name2date(file) for file in files]
+    df = pd.DataFrame(pd.value_counts(dates).sort_index(ascending=False), columns=["number_images"])
+    df.index.name = "date"
+    df.to_csv(STATS_DATE_FILE)
+
+
+if __name__ == "__main__":
+    main()
